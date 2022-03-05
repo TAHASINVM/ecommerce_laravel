0106f532d18b48b5e4411276f586dfd569f8ccb1
MOHAMMED TAHASIN VM <tahasinvm7545@gmail.com>
2022-03-06 01:58:14 +0530

    third commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,12 @@ Route::get('/', function () {
 });
 
 Route::get('admin',[AdminController::class,'index']);
-Route::post('admin/auth',[AdminController::class,'auth']);
+Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
+
+Route::group(['middleware'=>'admin_auth'],function(){
+    Route::get('admin/dashboard',[AdminController::class,'dashboard']);
+    Route::get('admin/category',[CategoryController::class,'index']);
+    Route::get('admin/manage_category',[CategoryController::class,'manage_category']);
+});
+
+
